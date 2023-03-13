9ad5f96b4fcbd5e835ff3d6ef0bc91c34a9f48cb
Adam Dresher <adamdresher@gmail.com>
2023-03-14 00:02:33 +0700

    setup file links and add sample content

--- cms.rb
+++ cms.rb
@@ -4,6 +4,6 @@ require 'tilt/erubis'
 
 get '/' do
   @root_dir = File.expand_path('..', __FILE__)
-  @files = Dir['*', base: "#{@dir}/public/files"]
+  @files = Dir['*', base: "#{@root_dir}/public/data"]
   erb :index
 end
