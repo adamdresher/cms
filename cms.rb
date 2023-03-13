aed86c04eea264f05a997196c7713e000c199e12
Adam Dresher <adamdresher@gmail.com>
2023-03-13 22:14:16 +0700

    setup view for root data dir

--- cms.rb
+++ cms.rb
@@ -3,5 +3,7 @@ require 'sinatra/reloader'
 require 'tilt/erubis'
 
 get '/' do
-  'Getting started.'
+  @dir = File.expand_path('..', __FILE__)
+  @files = Dir['*', base: "#{@dir}/public/files"]
+  erb :index
 end
