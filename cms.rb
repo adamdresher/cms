cf4569e83147daac7e6d8cfde6b58432621d232c
Adam Dresher <adamdresher@gmail.com>
2023-03-14 00:40:06 +0700

    add route to display file

--- cms.rb
+++ cms.rb
@@ -2,8 +2,21 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'tilt/erubis'
 
+before do
+  @root_dir = File.expand_path('..', __FILE__)
+end
+
 get '/' do
   @root_dir = File.expand_path('..', __FILE__)
   @files = Dir['*', base: "#{@root_dir}/public/data"]
-  erb :index
+  erb :index, layout: :layout
+end
+
+get '/:file' do
+  filename = params[:file]
+  # @file = File.read("/data/#{filename}")
+
+  @file = File.readlines("#{@root_dir}/public/data/#{filename}")
+
+  erb :file, layout: :layout
 end
