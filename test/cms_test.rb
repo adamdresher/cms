70cd7da0400ff37e119a6ba960af48fbe6007b26
Adam Dresher <adamdresher@gmail.com>
2023-03-17 15:37:57 +0700

    tests for editing file contents

--- test/cms_test.rb
+++ test/cms_test.rb
@@ -21,6 +21,8 @@ class CMSTest < Minitest::Test
     assert_includes last_response.body, 'about.txt'
     assert_includes last_response.body, 'changes.txt'
     assert_includes last_response.body, 'history.txt'
+    
+    # assert_includes edit links
   end
 
   def test_viewing_text_file
@@ -42,6 +44,8 @@ class CMSTest < Minitest::Test
   def test_nonexistent_file
     get '/nonexistent_file.txt'
 
+    root_path = File.expand_path('..', __FILE__)
+    refute_path_exists "#{root_path}/public/data/nonexistent_file.txt"
     assert_equal 302, last_response.status
 
     get last_response['Location']
@@ -57,4 +61,28 @@ class CMSTest < Minitest::Test
     refute_includes last_response.body, 'nonexistent_file.txt does not exist.'
     assert_includes last_response.body, 'about.txt'
   end
+end
+
+def test_edit_file_form
+  skip
+
+  get '/quotes.md/edit'
+
+  assert_equal 200, last_response.status
+  assert_equal 'text/html;charset=utf-8', last_response['Content-Type']
+  assert_includes last_response.body, '"We are what we repeatedly do.  Excellence, then, is not an act, but a habit"'
+end
+
+def test_edit_file
+  skip
+
+  post '/quotes.md/edit'
+
+  assert_equal 302, last_response.status
+  
+  get last_response['Location']
+
+  assert_equal 200, last_response.status
+  assert_includes last_response.body, 'quotes.md has been updated.'
+  assert_includes last_response.body, 'about.txt'
 end
